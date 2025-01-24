22e41bfefb4b3e63787955808bbbfe315cb51f9b
JacobWesthoff04 <mzufnz@gmail.com>
2025-01-23 22:46:35 -0600

    File reader made

--- HighScoresCSV.java
+++ HighScoresCSV.java
@@ -1,9 +1,23 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class HighScoresCSV {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        List<Player> players = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new FileReader("players.csv"));
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] data = line.split(",");
+            String initials = data[0];
+            int highScore = Integer.parseInt(data[1]);
+            players.add(new Player(initials, highScore));
+        }
+        br.close();
+
+        displayScores("player.csv");
     }
 }
 
