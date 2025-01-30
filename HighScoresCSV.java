2dbe48f441971755e04463d1fbabcf9d1cff39e5
Jacob <jw0962131@otc.edu>
2025-01-29 20:05:27 -0600

    for loop finished

--- HighScoresCSV.java
+++ HighScoresCSV.java
@@ -1,40 +1,23 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
 public class HighScoresCSV {
     public static void main(String[] args) throws IOException {
-        List<Player> players = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter your initials: ");
+            String initials = scanner.nextLine();
 
-        BufferedReader br = new BufferedReader(new FileReader("players.csv"));
-        String line;
-        while ((line = br.readLine()) != null) {
-            String[] data = line.split(",");
-            String initials = data[0];
-            int highScore = Integer.parseInt(data[1]);
-            players.add(new Player(initials, highScore));
+            System.out.print("Enter your score: ");
+            String score = scanner.nextLine();
+
+            try (FileWriter fw = new FileWriter("players.csv", true)) {
+                fw.append(initials).append(",");
+                fw.append(score).append("\n");
+            }
         }
-        br.close();
-
-        displayScores("player.csv");
-    }
-}
-
-class Player {
-    private String initials;
-    private int score;
-
-    public Player(String initials, int score) {
-        this.initials = initials;
-        this.score = score;
-    }
-
-    public String getInitials() {
-        return initials;
-    }
-
-    public int getScore() {
-        return score;
     }
 }
